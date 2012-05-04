332516b6ef02158d08a74407cc144c510173b717
Sho Hashimoto <hashimoto@shokai.org>
2012-05-04 23:16:45 +0900

    separate controller main with api

---
+++ WebUI/controllers/api.rb
@@ -0,0 +1,26 @@
+
+before '/*.json' do
+  content_type 'application/json'
+end
+
+get '/chat.json' do
+  uri = URI.parse @@conf['gateway']
+  res = nil
+  Net::HTTP.start(uri.host, uri.port) do |http|
+    res = http.get(uri.path)
+  end
+  status 200
+  @mes = res.body
+end
+
+post '/chat.json' do
+  @name = params['name']
+  @body = params['body']
+  uri = URI.parse @@conf['gateway']
+  res = nil
+  Net::HTTP.start(uri.host, uri.port) do |http|
+    res = http.post(uri.path, "<#{@name}> #{@body}")
+  end
+  status 200
+  @mes = {:api => res.body}.to_json
+end
